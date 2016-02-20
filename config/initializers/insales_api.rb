c7bb488462fbc06a990ee36baa70b5eaf613f05f
stap780 <panaet80@mail.ru>
2016-02-20 15:36:34 -0500

    Change deploy

--- config/initializers/insales_api.rb
+++ config/initializers/insales_api.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 MyApp.api_key            = 'app1'
 MyApp.api_secret         = '9b80e05c79004aef0b077ba6d4ed220a'
-#MyApp.api_host           = 'http://188.166.111.118:3000/'
-#MyApp.api_autologin_path = 'session/autologin'
-MyApp.api_autologin_url  = 'http://104.236.54.103/session/autologin'
+MyApp.api_host           = 'http://188.166.111.118:3000/'
+MyApp.api_autologin_path = 'session/autologin'
+MyApp.api_autologin_url  = 'http://188.166.111.118:3000/session/autologin'
 
