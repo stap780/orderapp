bc29a38dca6bf2058967580bf5711674dcf11f91
stap780 <panaet80@mail.ru>
2016-02-24 10:37:28 -0500

    my litle changes

--- config/routes.rb
+++ config/routes.rb
@@ -150,16 +150,16 @@ InsalesApp::Application.routes.draw do
 
   root :to => 'main#index'
 
-  resource  :session do
-    collection do
-      get :autologin
-    end
-  end
-
-  get '/install',      :to => 'insales_app#install',   :as => :install
-  get '/uninstall',    :to => 'insales_app#uninstall', :as => :uninstall
-  get '/login',        :to => 'sessions#new',          :as => :login
-  get '/main',         :to => 'main#index',            :as => :main           
+  # resource  :session do
+#     collection do
+#       get :autologin
+#     end
+#   end
+# 
+#   get '/install',      :to => 'insales_app#install',   :as => :install
+#   get '/uninstall',    :to => 'insales_app#uninstall', :as => :uninstall
+#   get '/login',        :to => 'sessions#new',          :as => :login
+#   get '/main',         :to => 'main#index',            :as => :main           
         
    
   #get ':controller/:action/:id'
