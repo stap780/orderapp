22d40b54b70b25f82ae35f2eaf3a3f93d89a520a
stap780 <panaet80@mail.ru>
2016-02-24 13:47:53 -0500

    my litle changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
     @products = @search.result.paginate(page: params[:page], per_page: 50)
     @totalproducts = Product.count
     @totalproductssku = Product.where("cast(sku as integer) > 0").count
-    @zeroquantity = Product.where("cast(quantity as integer) == 0").count
+    @zeroquantity = Product.where("cast(quantity as integer) = 0").count
     @qnotzero = Product.where("cast(quantity as integer) > 0").count
     @products_all = Product.all
     respond_to do |format|
