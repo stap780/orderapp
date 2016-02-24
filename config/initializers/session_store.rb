8e9c17e00ef32d513950b7528f8e3cd15eb49c87
stap780 <panaet80@mail.ru>
2016-02-24 09:29:20 -0500

    my litle changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-InsalesApp::Application.config.session_store :cookie_store, :key => '_insales_app_session'
+InsalesApp::Application.config.session_store :cookie_store, :key => '_insales_app_session', :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
